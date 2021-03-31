27b48591d915d8ab5d502a1df1ae8d76162fb8eb
isadoraoliveiraa <81166225+isadoraoliveiraa@users.noreply.github.com>
2021-03-31 11:42:44 -0300

    Create atividade2.py

---
+++ atividade2.py
@@ -0,0 +1,37 @@
+def calcula_soma(lista):
+  lista = []
+  soma_da_lista = sum(lista)
+  i = 0
+  for i in lista (lista.len):
+    soma_da_lista += lista[i]
+  return soma_da_lista
+  print( soma_da_lista )
+  
+  
+def converte_entrada(texto):
+  lista = list(texto.split(" "))
+  return lista
+  
+str1 = input('')
+print (converte_entrada(str1))
+
+
+def processa_numeros (lista):
+ lista_de_numeros = []
+ i = 0
+ soma = 0
+ lista_de_numero (entrada)
+ i += 1
+
+
+ return (soma)
+   
+  def main():
+  entrada = input('Digite números separados por espaço ')
+  
+  lista_com_numeros = converte_entrada(entrada)
+  media = processa_numeros(lista_com_numeros)
+  
+  media = media[0] / media[1]
+
+  print(f'A média desses números é {media:.2f}')
