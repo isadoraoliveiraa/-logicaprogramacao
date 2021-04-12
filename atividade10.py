de7d2ab539ed69086ab4041658956caba552607b
isadoraoliveiraa <81166225+isadoraoliveiraa@users.noreply.github.com>
2021-04-12 14:05:34 -0300

    Create atividade10.py

---
+++ atividade10.py
@@ -0,0 +1,34 @@
+def exercicio1():
+  numero_n = int(input())
+  i = 0
+  while numero_n > i:
+    if i % 5 == 0 and i % 3 != 0:
+      print(i)
+    i += 1
+    
+def exercicio2():
+  numero = input("Digite seus numeros: \r\n")
+  lista = []
+  lista_n = numero.split()
+  for numero in lista_n:
+    lista.append(int(numero))
+  return lista
+
+def exercicio3():
+  numeros = input("Digite seus numeros: \r\n")
+  lista = []
+  lista_n = numeros.split()
+  soma_num = 0
+  for numeros in lista_n:
+    lista.append(int(numeros))
+    if int(numeros) > 5:
+      soma_num += 1
+      return soma_num
+      
+def exercicio4():
+  opcao = input()
+  while opcao != 'd':
+    if opcao == 'a':
+      print("PSG")
+    elif opcao == 'b':
+      print("BAYER")
